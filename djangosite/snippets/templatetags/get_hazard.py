113cfad842aa2d38395c11d6938acb6a81039696
Peter Barkley <peter.barkley@gmail.com>
2017-09-17 21:34:23 -1000

    Potential hazard now works. Run a migration to use

--- djangosite/snippets/templatetags/get_hazard.py
+++ djangosite/snippets/templatetags/get_hazard.py
@@ -1,11 +1,12 @@
 
 from django import template
-from . import getattribute
-
 register = template.Library()
 
 
 @register.simple_tag
 def get_hazard(hazard_model, hazard_arg, site_value):
+    hazard_value = getattr(hazard_model, hazard_arg)
+    if site_value is not None and hazard_value is not None and site_value >= hazard_value:
+        return "Yes"
     return ""
     
