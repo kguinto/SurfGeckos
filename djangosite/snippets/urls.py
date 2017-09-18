d903c718a008f47020e7fc9020986d7cbb00b509
Peter Barkley <peter.barkley@gmail.com>
2017-09-17 20:34:09 -1000

    remove wkhtmltopdf

--- djangosite/snippets/urls.py
+++ djangosite/snippets/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from wkhtmltopdf.views import PDFTemplateView
 from . import views
 
 app_name = 'snippets'
@@ -8,8 +7,6 @@ urlpatterns = [
     url(r'^sitequery/(?P<pk>[0-9]+)/$', views.SiteQueryDetail.as_view(), name='sitequery_detail'),
     url(r'^sitequery/(?P<sitequery_id>[0-9]+)/$', views.sitequery, name='sitequery'),
     url(r'^sitequery/create/$', views.create_sitequery, name='create_sitequery'),
-    url(r'^sitequery/(?P<pk>[0-9]+)/pdf/$', views.sitequery_pdf,
-        name='sitequery_pdf'),
     url(r'^sitecontaminant/(?P<sitecontaminant_id>[0-9]+)/$', views.sitecontaminant, name='sitecontaminant'),
     url(r'^sitecontaminant/create/(?P<sitequery_id>[0-9]+)$', views.create_sitecontaminant, name='create_sitecontaminant'),
     url(r'^contaminants/$', views.ContaminantList.as_view(), name='contaminant_list'),
